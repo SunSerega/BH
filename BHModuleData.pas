ecddfb8737866715434e199f83e219e5ae1bf28d
Sun Serega <latchenko3@yandex.ru>
2019-01-19 20:42:04 +0200

    Better module loading Now unused .dll would auto unload

--- BHModuleData.pas
+++ BHModuleData.pas
@@ -1,5 +1,7 @@
 ﻿library BHModuleData;
 
+interface
+
 {$reference System.Windows.Forms.dll}
 {$reference System.Drawing.dll}
 uses System.Windows.Forms;
@@ -7,271 +9,10 @@ uses System.Drawing;
 
 uses System.Reflection;
 
+uses MiscData;
+
 type
   BHModule = abstract class
-    
-    {$region static}
-    
-    private static All := new Dictionary<string, BHModule>;
-    
-    private static function GetAllBaseTypes(t: System.Type): sequence of System.Type;
-    begin
-      var base := t.BaseType;
-      if base=nil then exit;
-      yield base;
-      yield sequence GetAllBaseTypes(base);
-    end;
-    
-    //ToDo в следующем билде можно убрать
-    private static function _GetModuleById(id: string): BHModule := All[id];
-    
-    private static property Item[id: string]: BHModule read _GetModuleById; default;
-    
-    ///Collection of all modules
-    public static property Modules: System.Collections.Generic.IReadOnlyCollection<BHModule> read All.Values;
-    
-    private static function LoadSettings(var Settings: Dictionary<string, Dictionary<string, string>>): boolean;
-    begin
-      
-      if System.IO.File.Exists('settings (backup).dat') then
-        case MessageBox.Show(
-          
-          $'Probably, last time settings were saving, issue occurred.{#10}' +
-          $'Load backup?',
-          
-          $'Settings backup found',
-          
-          MessageBoxButtons.YesNo
-        ) of
-          DialogResult.Yes:
-          begin
-            System.IO.File.Copy('settings (backup).dat', 'settings.dat', true);
-            System.IO.File.Delete('settings (backup).dat');
-          end;
-        end;
-      
-      Settings := new Dictionary<string, Dictionary<string, string>>;
-      
-      foreach var l in ReadLines('settings.dat') do
-      begin
-        if l='' then continue;
-        
-        var ss := l.Split(new char[]('='), 2);
-        if ss.Length<>2 then
-        begin
-          Result := true;
-          ss := new string[](ss[0], '');
-        end;
-        
-        var key := ss[0].Trim(' ', #9).Split(new char[](':'), 2);
-        if key.Length<>2 then
-        begin
-          Result := true;
-          key := new string[](key[0], '');
-        end;
-        
-        var MName := key[0].Trim(' ', #9);
-        var SName := key[1].Trim(' ', #9);
-        var SVal := ss[1].Trim(' ', #9);
-        
-        if not Settings.ContainsKey(MName) then Settings[MName] := new Dictionary<string, string>;
-        var MSettings := Settings[MName];
-        
-        if MSettings.ContainsKey(SName) then
-          if MSettings[SName] = SVal then
-            Result := true else
-            case MessageBox.Show(
-              
-              $'Key "{MName}:{SName}" was found multiple times in "settings.dat" file.{#10}' +
-              $'First value is "{MSettings[SName]}".{#10}' +
-              $'New value is "{SVal}".{#10}' +
-              $'Rewrite first value with new one?',
-              
-              $'Conflicting settings keys',
-              
-              MessageBoxButtons.YesNo
-            ) of
-              DialogResult.Yes: Result := true;
-              else continue;//foreach var l
-            end;
-        
-        MSettings[SName] := SVal;
-        
-      end;
-      
-    end;
-    
-    private static procedure SaveSettings(Settings: Dictionary<string, Dictionary<string, string>>);
-    begin
-      
-      System.IO.File.Copy('settings.dat', 'settings (backup).dat', true);
-      var sw := System.IO.File.CreateText('settings.dat');
-      
-      foreach var kvp1 in Settings do
-        foreach var kvp2 in kvp1.Value do
-          if not kvp2.Key.StartsWith(#0) then
-            sw.WriteLine($'{kvp1.Key}:{kvp2.Key}={kvp2.Value}');
-      
-      sw.Close;
-      System.IO.File.Delete('settings (backup).dat');
-      
-    end;
-    
-    private static NewSettings := new Dictionary<string, Dictionary<string, string>>;
-    
-    public static constructor :=
-    try
-      
-      {$region Load Modules}
-      
-      foreach var t: System.Type in
-        System.IO.Directory.EnumerateDirectories(System.IO.Path.GetFullPath('Modules'))
-        .SelectMany(
-          fld->
-          System.IO.Directory.EnumerateFiles(fld)
-          .Where(fname->fname.EndsWith('.dll'))
-          .SelectMany(fname->
-          {}Assembly.LoadFile(fname)
-          {}.GetExportedTypes
-          {}.Where(
-          {}  t->
-          {}  t.IsSubclassOf(typeof(BHModule))
-          {})
-          )
-        )
-      do
-      begin
-        if t.IsAbstract then continue;
-        var constr := t.GetConstructor(System.Type.EmptyTypes);
-        if constr=nil then continue;
-        var nm := BHModule(constr.Invoke(new object[0]));
-        
-        if All.ContainsKey(nm.Name) then
-        begin
-          MessageBox.Show(
-            
-            $'Every module must have unique name{#10}' +
-            $'But more than one "{nm.Name}" modules was found{#10}' +
-            $'Press OK to exit BH',
-            
-            $'Conflicting module name''s'
-            
-          );
-          Halt;
-        end;
-        
-        All.Add(nm.Name, nm);
-      end;
-      
-      {$endregion Load Modules}
-      
-      var Settings: Dictionary<string, Dictionary<string, string>>;
-      var ReSaveSettings := LoadSettings(Settings);
-      
-      {$region Use settings on modules}
-      
-      foreach var m in Modules do
-      begin
-        if not Settings.ContainsKey(m.Name) then
-        begin
-          ReSaveSettings := true;
-          Settings[m.Name] := new Dictionary<string, string>;
-        end;
-        
-        ReSaveSettings := m.ApplySettings(Settings[m.Name]) or ReSaveSettings;
-        
-        Settings[m.Name][#0'=used'] := nil;
-      end;
-      
-      foreach var kvp in Settings.ToList do
-        if not kvp.Value.ContainsKey(#0'=used') then
-          case MessageBox.Show(
-            
-            $'Module "{kvp.Key}" was not found.{#10}' +
-            $'Delete it''s settings?{#10}',
-            
-            $'Unused settings keys',
-            
-            MessageBoxButtons.YesNo
-          ) of
-            DialogResult.Yes:
-            begin
-              ReSaveSettings := true;
-              Settings.Remove(kvp.Key);
-            end;
-            else continue;
-          end;
-      
-      {$endregion Use settings on modules}
-      
-      if ReSaveSettings then
-        SaveSettings(Settings);
-      
-    except
-      on e: Exception do
-      begin
-        try
-          MessageBox.Show(
-            _ObjectToString(e),
-            'critical error initializing modules'
-          );
-        except
-          MessageBox.Show(
-            '*error geting error text*',
-            'critical error initializing modules'
-          );
-        end;
-        Halt;
-      end;
-    end;
-    
-    protected procedure OverrideSetting(SName, SVal: string);
-    begin
-      var MName := self.Name;
-      if not NewSettings.ContainsKey(MName) then NewSettings[MName] := new Dictionary<string, string>;
-      NewSettings[MName][SName] := SVal;
-    end;
-    
-    private static procedure SaveNewSettings;
-    begin
-      
-      var Settings: Dictionary<string, Dictionary<string, string>>;
-      var ReSaveSettings := LoadSettings(Settings);
-      
-      foreach var kvp1 in NewSettings do
-      begin
-        if not Settings.ContainsKey(kvp1.Key) then Settings[kvp1.Key] := new Dictionary<string, string>;
-        
-        foreach var kvp2 in kvp1.Value do
-          if
-            (not Settings[kvp1.Key].ContainsKey(kvp2.Key)) or
-            (Settings[kvp1.Key][kvp2.Key] <> kvp2.Value)
-          then
-          begin
-            ReSaveSettings := true;
-            Settings[kvp1.Key][kvp2.Key] := kvp2.Value;
-          end;
-        
-      end;
-      NewSettings.Clear;
-      
-      if ReSaveSettings then
-        SaveSettings(Settings);
-      
-    end;
-    
-    public static procedure GetReadyForExit;
-    begin
-      
-      foreach var m in Modules do
-        if m.Runing then
-          m.ShutDown;
-      
-      SaveNewSettings;
-      
-    end;
-    
-    {$endregion static}
     
     {$region MainBody}
     
@@ -290,6 +31,8 @@ type
       OverrideSetting('Active', value.ToString);
       
     end;
+    
+    private source_file: string;
     
     
     
@@ -361,6 +104,146 @@ type
     
     {$endregion MainBody}
     
+    {$region static}
+    
+    private static All := new Dictionary<string, BHModule>;
+    
+    ///Find's module by it's name
+    ///This is a default static index property
+    public static property Module[id: string]: BHModule read All[id]; default;
+    
+    ///Collection of all modules
+    public static property Modules: System.Collections.Generic.IReadOnlyCollection<BHModule> read All.Values;
+    
+    static constructor;
+    
+    private static function LoadSettings(var Settings: Dictionary<string, Dictionary<string, string>>): boolean;
+    begin
+      
+      if System.IO.File.Exists('settings (backup).dat') then
+      begin
+        System.IO.File.Copy('settings (backup).dat', 'settings.dat', true);
+        System.IO.File.Delete('settings (backup).dat');
+      end;
+      
+      Settings := new Dictionary<string, Dictionary<string, string>>;
+      
+      foreach var l in ReadLines('settings.dat') do
+      begin
+        if l='' then continue;
+        
+        var ss := l.Split(new char[]('='), 2);
+        if ss.Length<>2 then
+        begin
+          Result := true;
+          ss := new string[](ss[0], '');
+        end;
+        
+        var key := ss[0].Split(new char[](':'), 2);
+        if key.Length<>2 then
+        begin
+          Result := true;
+          key := new string[](key[0], '');
+        end;
+        
+        var MName := key[0].Trim(' ', #9);
+        var SName := key[1].Trim(' ', #9);
+        var SVal := ss[1].Trim(' ', #9);
+        
+        if not Settings.ContainsKey(MName) then Settings[MName] := new Dictionary<string, string>;
+        var MSettings := Settings[MName];
+        
+        if MSettings.ContainsKey(SName) then
+          if MSettings[SName] = SVal then
+            Result := true else
+            case MessageBox.Show(
+              
+              $'Key "{MName}:{SName}" was found multiple times in "settings.dat" file.{#10}' +
+              $'First value is "{MSettings[SName]}".{#10}' +
+              $'New value is "{SVal}".{#10}' +
+              $'Rewrite first value with new one?{#10}' +
+              $'(Press "cancel" to exit BH without saving)',
+              
+              $'Conflicting settings keys',
+              
+              MessageBoxButtons.YesNoCancel
+            ) of
+              DialogResult.Yes: Result := true;
+              DialogResult.Cancel: Halt;
+              else continue;//foreach var l
+            end;
+        
+        MSettings[SName] := SVal;
+        
+      end;
+      
+    end;
+    
+    private static procedure SaveSettings(Settings: Dictionary<string, Dictionary<string, string>>);
+    begin
+      
+      System.IO.File.Copy('settings.dat', 'settings (backup).dat', true);
+      var sw := System.IO.File.CreateText('settings.dat');
+      
+      foreach var kvp1 in Settings do
+        foreach var kvp2 in kvp1.Value do
+          sw.WriteLine($'{kvp1.Key}:{kvp2.Key}={kvp2.Value}');
+      
+      sw.Close;
+      System.IO.File.Delete('settings (backup).dat');
+      
+    end;
+    
+    private static NewSettings := new Dictionary<string, Dictionary<string, string>>;
+    
+    protected procedure OverrideSetting(SName, SVal: string);
+    begin
+      var MName := self.Name;
+      if not NewSettings.ContainsKey(MName) then NewSettings[MName] := new Dictionary<string, string>;
+      NewSettings[MName][SName] := SVal;
+    end;
+    
+    private static procedure SaveNewSettings;
+    begin
+      
+      var Settings: Dictionary<string, Dictionary<string, string>>;
+      var ReSaveSettings := LoadSettings(Settings);
+      
+      foreach var kvp1 in NewSettings do
+      begin
+        if not Settings.ContainsKey(kvp1.Key) then Settings[kvp1.Key] := new Dictionary<string, string>;
+        
+        foreach var kvp2 in kvp1.Value do
+          if
+            (not Settings[kvp1.Key].ContainsKey(kvp2.Key)) or
+            (Settings[kvp1.Key][kvp2.Key] <> kvp2.Value)
+          then
+          begin
+            ReSaveSettings := true;
+            Settings[kvp1.Key][kvp2.Key] := kvp2.Value;
+          end;
+        
+      end;
+      NewSettings.Clear;
+      
+      if ReSaveSettings then
+        SaveSettings(Settings);
+      
+    end;
+    
+    public static procedure GetReadyForExit;
+    begin
+      
+      foreach var m in Modules do
+        if m.Runing then
+          m.ShutDown;
+      
+      SaveNewSettings;
+      
+    end;
+    
+    {$endregion static}
+    
     {$region Misc}
     
     ///Return's $"BHModule[\"{Name}\"]"
@@ -372,4 +255,138 @@ type
     
   end;
   
+implementation
+
+procedure TryLoadAssembly;
+begin
+  var fname := string(System.AppDomain.CurrentDomain.GetData('fname'));
+  
+  System.AppDomain.CurrentDomain.SetData(
+    'tps',
+    Assembly.LoadFile(fname)
+    .GetExportedTypes
+    .Where(t -> not t.IsAbstract)
+    .Where(t -> t.IsSubclassOf(typeof(BHModule)))
+    .Where(t -> t.GetConstructor(System.Type.EmptyTypes) <> nil)
+    .Select(t->t.FullName)
+    .ToHashSet
+  );
+  
+end;
+
+static constructor BHModule.Create :=
+try
+  
+  {$region Load Modules}
+  
+  foreach var nm: BHModule in
+    System.IO.Directory.EnumerateDirectories(System.IO.Path.GetFullPath('Modules'))
+    .SelectMany(
+      fld->
+      System.IO.Directory.EnumerateFiles(fld, '*.dll')
+      .SelectMany(
+        fname->
+        begin
+          var ad := System.AppDomain.CreateDomain($'Domain for BH modules from file {fname}');
+          
+          ad.SetData('fname', fname);
+          ad.DoCallBack(TryLoadAssembly);
+          var tps := HashSet&<string>(ad.GetData('tps'));
+          System.AppDomain.Unload(ad);
+          
+          Result :=
+            Assembly.LoadFile(fname)
+            .GetExportedTypes
+            .Where(t -> tps.Contains(t.FullName))
+            .Select(
+              t ->
+              begin
+                Result :=
+                  BHModule(
+                    t.GetConstructor(
+                      System.Type.EmptyTypes
+                    ).Invoke(new object[0])
+                  );
+                Result.source_file := fname;
+              end
+            );
+          
+        end
+      )
+    )
+  do
+  begin
+    
+    if All.ContainsKey(nm.Name) then
+    begin
+      MessageBox.Show(
+        
+        $'Theese 2 modules have name "{nm.Name}"{#10}' +
+        #10 +
+        $'Class {All[nm.Name].GetType.FullName}{#10}' +
+        $'From file "{All[nm.Name].source_file}"{#10}' +
+        #10 +
+        $'Class {nm.GetType.FullName}{#10}' +
+        $'From file "{nm.source_file}"{#10}' +
+        #10 +
+        $'Press OK to exit BH',
+        
+        $'Conflicting module names'
+        
+      );
+      Halt;
+    end;
+    
+    All.Add(nm.Name, nm);
+  end;
+  
+  {$endregion Load Modules}
+  
+  var Settings: Dictionary<string, Dictionary<string, string>>;
+  var ReSaveSettings := LoadSettings(Settings);
+  
+  {$region Use settings on modules}
+  
+  foreach var m in Modules do
+  begin
+    if not Settings.ContainsKey(m.Name) then
+    begin
+      ReSaveSettings := true;
+      Settings[m.Name] := new Dictionary<string, string>;
+    end;
+    
+    ReSaveSettings := m.ApplySettings(Settings[m.Name]) or ReSaveSettings;
+    
+  end;
+  
+  var names := Modules.Select(m->m.Name).ToHashSet;
+  
+  foreach var kvp in Settings.ToList do
+    if not names.Contains(kvp.Key) then
+      case MessageBox.Show(
+        
+        $'Module "{kvp.Key}" was not found.{#10}' +
+        $'Delete it''s settings?',
+        
+        $'Unused settings keys',
+        
+        MessageBoxButtons.YesNo
+      ) of
+        DialogResult.Yes:
+        begin
+          ReSaveSettings := true;
+          Settings.Remove(kvp.Key);
+        end;
+        else continue;
+      end;
+  
+  {$endregion Use settings on modules}
+  
+  if ReSaveSettings then
+    SaveSettings(Settings);
+  
+except
+  on e: Exception do ShowCriticalError(e, 'critical error initializing modules');
+end;
+
 end.
---
+++ MiscData.pas
@@ -0,0 +1,48 @@
+﻿unit MiscData;
+
+interface
+
+{$reference System.Windows.Forms.dll}
+uses System.Windows.Forms;
+
+procedure ShowCriticalError(e: Exception; title: string);
+
+implementation
+
+procedure TryShowMB(s1, s2: string) :=
+MessageBox.Show(s2, s1);
+
+procedure TrySaveLog(s1, s2: string);
+begin
+  var fname := 'Critical Error.txt';
+  WriteAllText(fname, Concat(s1, ':'#10#10, s2));
+  Exec(fname);
+end;
+
+
+function GetText1(e: Exception) := _ObjectToString(e);
+function GetText2(e: Exception) := e.Message+#10'*error getting full error text*';
+function GetText3(e: Exception) := '*error geting error text*';
+
+
+procedure ShowCriticalError(e: Exception; title: string);
+begin
+  var text: string;
+  
+  foreach var gtf in Arr(GetText1, GetText2, GetText3) do
+  try
+    text := gtf(e);
+    break;
+  except
+  end;
+  
+  foreach var sm in Arr(TryShowMB, TrySaveLog) do
+  try
+    sm(title, text);
+    Halt;
+  except
+  end;
+  
+end;
+
+end.
